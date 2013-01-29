c05b2502afaca400f204e2d2f7b0b99661abf861
hiepkhachbk <thaichuong159@gmail.com>
2013-01-28 21:50:09 -0800

    Committed Unit test for estimateAgeTest()

---
+++ api/src/test/java/org/openmrs/module/registration/util/RegistrationUtilsTest.java
@@ -0,0 +1,28 @@
+package org.openmrs.module.registration.util;
+
+import java.text.ParseException;
+import java.util.Calendar;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RegistrationUtilsTest
+{
+    public String date;
+    
+    @Before
+    public void before() throws ParseException{
+        Calendar cal = Calendar.getInstance();
+        // cal.set( 2013, Calendar.JANUARY, 28 );
+        date = cal.get(cal.DATE) + "/" + (cal.get( cal.MONTH ) + 1) + "/" + cal.get( cal.YEAR );
+    }
+    
+    @Test
+    public void estimateAgeTest() throws ParseException{
+        Assert.assertEquals( "~0 day ", RegistrationUtils.estimateAge( date ) );
+}
+    
+   
+
+}
